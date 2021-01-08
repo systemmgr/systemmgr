9c9c285fd64f161b63a881be1f6fe706205be87a
Jason <jason@casjay.net>
2021-01-08 14:55:51 -0500

    🦈🏠🐜❗ Initial Commit ❗🐜🦈🏠

---
+++ README.md
@@ -0,0 +1,42 @@
+## Template  
+  
+Template is a shell designed for interactive use  
+  
+Automatic install/update:
+
+```shell
+bash -c "$(curl -LSs https://github.com/systemmgr/Template/raw/master/install.sh)"
+```
+
+Manual install:
+  
+requires:
+
+Debian based:
+
+```shell
+apt install Template
+```  
+
+Fedora Based:
+
+```shell
+yum install Template
+```  
+
+Arch Based:
+
+```shell
+pacman -S Template
+```  
+
+MacOS:  
+
+```shell
+brew install Template
+```
+  
+<p align=center>
+  <a href="https://wiki.archlinux.org/index.php/Template" target="_blank" rel="noopener noreferrer">Template wiki</a>  |  
+  <a href="http://Template.sourceforge.net" target="_blank" rel="noopener noreferrer">Template site</a>
+</p>  
---
+++ install.sh
@@ -0,0 +1,149 @@
+#!/usr/bin/env bash
+
+APPNAME="template"
+USER="${SUDO_USER:-${USER}}"
+HOME="${USER_HOME:-${HOME}}"
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# @Author          : Jason
+# @Contact         : casjaysdev@casjay.net
+# @File            : install.sh
+# @Created         : Fr, Aug 28, 2020, 00:00 EST
+# @License         : WTFPL
+# @Copyright       : Copyright (c) CasjaysDev
+# @Description     : installer script for Template
+#
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+# Set functions
+
+SCRIPTSFUNCTURL="${SCRIPTSAPPFUNCTURL:-https://github.com/casjay-dotfiles/scripts/raw/master/functions}"
+SCRIPTSFUNCTDIR="${SCRIPTSAPPFUNCTDIR:-/usr/local/share/CasjaysDev/scripts}"
+SCRIPTSFUNCTFILE="${SCRIPTSAPPFUNCTFILE:-app-installer.bash}"
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+if [ -f "$SCRIPTSFUNCTDIR/functions/$SCRIPTSFUNCTFILE" ]; then
+  . "$SCRIPTSFUNCTDIR/functions/$SCRIPTSFUNCTFILE"
+elif [ -f "$HOME/.local/share/CasjaysDev/functions/$SCRIPTSFUNCTFILE" ]; then
+  . "$HOME/.local/share/CasjaysDev/functions/$SCRIPTSFUNCTFILE"
+else
+  curl -LSs "$SCRIPTSFUNCTURL/$SCRIPTSFUNCTFILE" -o "/tmp/$SCRIPTSFUNCTFILE" || exit 1
+  . "/tmp/$SCRIPTSFUNCTFILE"
+fi
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+system_installdirs
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+# Make sure the scripts repo is installed
+
+scripts_check
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+# Defaults
+APPNAME="${APPNAME:-template}"
+APPDIR="${APPDIR:-$SHARE/CasjaysDev/systemmgr}/$APPNAME"
+REPO="${SYSTEMMGRREPO:-https://github.com/systemmgr}/${APPNAME}"
+REPORAW="${REPORAW:-$REPO/raw}"
+APPVERSION="$(curl -LSs $REPORAW/master/version.txt)"
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+# dfmgr_install fontmgr_install iconmgr_install pkmgr_install systemmgr_install thememgr_install wallpapermgr_install
+
+systemmgr_install
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+# Script options IE: --help
+
+show_optvars "$@"
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+# end with a space
+
+APP=""
+PERL=""
+PYTH=""
+PIPS=""
+CPAN=""
+GEMS=""
+
+# install packages - useful for package that have the same name on all oses
+install_packages $APP
+
+# install required packages using file
+install_required $APP
+
+# check for perl modules and install using system package manager
+install_perl $PERL
+
+# check for python modules and install using system package manager
+install_python $PYTH
+
+# check for pip binaries and install using python package manager
+install_pip $PIPS
+
+# check for cpan binaries and install using perl package manager
+install_cpan $CPAN
+
+# check for ruby binaries and install using ruby package manager
+install_gem $GEMS
+
+# Other dependencies
+dotfilesreq
+dotfilesreqadmin
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+# Ensure directories exist
+
+ensure_dirs
+ensure_perms
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+# Main progam
+
+if [ -d "$APPDIR/.git" ]; then
+  execute \
+    "git_update $APPDIR" \
+    "Updating $APPNAME configurations"
+else
+  execute \
+    "backupapp && \
+        git_clone -q $REPO/$APPNAME $APPDIR" \
+    "Installing $APPNAME configurations"
+fi
+
+# exit on fail
+failexitcode
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+# run post install scripts
+
+run_postinst() {
+  systemmgr_run_postinst
+}
+
+execute \
+  "run_postinst" \
+  "Running post install scripts"
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+# create version file
+
+systemmgr_install_version
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+# exit
+run_exit
+
+# end
